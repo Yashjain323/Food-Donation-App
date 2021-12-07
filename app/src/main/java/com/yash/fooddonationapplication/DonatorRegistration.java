3ed8b74485f94ac8173d5a624e39979fa404c1a3
Yash <yashjainhaha@gmail.com>
2021-12-07 23:46:00 +0530

    initial commit

---
+++ app/src/main/java/com/yash/fooddonationapplication/DonatorRegistration.java
@@ -0,0 +1,14 @@
+package com.yash.fooddonationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonatorRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donator_registration);
+    }
+}
