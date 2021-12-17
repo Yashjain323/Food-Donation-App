ad01c6efbde66e229f39dccc0deea7dfab276b36
Yash <yashjainhaha@gmail.com>
2021-12-18 00:48:32 +0530

    new activities

---
+++ app/src/main/java/com/yash/fooddonationapplication/OrganisationSetup.java
@@ -0,0 +1,14 @@
+package com.yash.fooddonationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrganisationSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organisation_setup);
+    }
+}
