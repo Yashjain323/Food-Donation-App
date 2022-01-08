101e51a360e71ec515afcaabddf5d8ea46139b95
Yash <yashjainhaha@gmail.com>
2022-01-08 21:36:46 +0530

    shobhit bhai ke liye

---
+++ app/src/main/java/com/yash/fooddonationapplication/DonorReciepientHomePage.java
@@ -0,0 +1,14 @@
+package com.yash.fooddonationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonorReciepientHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_reciepient_home_page);
+    }
+}
