a304e40f2b9d33dd4472dd5480a1d1af7b21e177
Yash <yashjainhaha@gmail.com>
2022-01-08 21:34:22 +0530

    Tikam dada ke liye maal

---
+++ app/src/main/java/com/yash/fooddonationapplication/DonorFoodDetailsPost.java
@@ -0,0 +1,14 @@
+package com.yash.fooddonationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonorFoodDetailsPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_food_details_post);
+    }
+}
