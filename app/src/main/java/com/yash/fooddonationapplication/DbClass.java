dba7f48b9df1b62258cf480c992eea9a7feffdb3
Yash <yashjainhaha@gmail.com>
2022-01-15 22:57:14 +0530

    Database connection established

--- app/src/main/java/com/yash/fooddonationapplication/DbClass.java
+++ app/src/main/java/com/yash/fooddonationapplication/DbClass.java
@@ -2,35 +2,107 @@ package com.yash.fooddonationapplication;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
-public class DbClass extends SQLiteOpenHelper {
-    public DbClass(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+public class DbClass {
+    private SQLiteDatabase db;
+    /*
+ Next we have a public static final string for
+ each row/table that we need to refer to both
+ inside and outside this class
+ */
+    public static final String TABLE_ROW_ID = "_id";
+    public static final String TABLE_ROW_NAME = "name";
+    public static final String TABLE_ROW_ADDRESS = "address";
+    public static final String TABLE_ROW_DESC = "description";
+    public static final String TABLE_ROW_PHONE = "phone";
+    //name VARCHAR, address VARCHAR, description VARCHAR,phone INT(10))
+    /*
+    Next we have a private static final strings for
+    each row/table that we need to refer to just
+    inside this class
+    */
+    private static final String DB_NAME = "food_donation_db";
+    private static final int DB_VERSION = 1;
+    private static final String TABLE_N_AND_A = "reciepient_info";
+
+    public DbClass(Context context) {
+        // Create an instance of our internal
+        //CustomSQLiteOpenHelper class
+        CustomSQLiteOpenHelper helper = new CustomSQLiteOpenHelper(context);
+        // Get a writable database
+        db = helper.getWritableDatabase();
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-
+    // Insert a record
+    public void insert(String name, String address, String desc, String phone) {
+        // Add all the details to the table
+        String query = "INSERT INTO " + TABLE_N_AND_A + " (" + TABLE_ROW_NAME + ", " +
+                TABLE_ROW_ADDRESS + "," + TABLE_ROW_DESC + ","+ TABLE_ROW_PHONE +") " +
+                "VALUES (" + "'" + name + "'" + ", " + "'" + address + "'" + "," + "'" + desc + "'" + ", " + "'" + phone + "'"+ "); ";
+        Log.i("insert() = ", query);
+        db.execSQL(query);
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+    public void delete(String name) {
+        // Delete the details from the table if already exists
+        String query = "DELETE FROM " + TABLE_N_AND_A + " WHERE " + TABLE_ROW_NAME + " = '" + name + "';";
+        Log.i("delete() = ", query);
+        db.execSQL(query);
     }
 
-    public void insertContact(String name, String address, String phone, String desc) {
+    // Get all the records
+    public Cursor selectAll() {
+        Cursor c = db.rawQuery("SELECT *" + " from " +
+                TABLE_N_AND_A, null);
+        return c;
+    }
 
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("address", address);
-        contentValues.put("description", phone);
-        contentValues.put("phone", desc);
-        db.insert("OrgTable", null, contentValues);
+    // Find a specific record
+    public Cursor searchName(String name) {
+        String query = "SELECT " +
+                TABLE_ROW_ID + ", " +
+                TABLE_ROW_NAME +
+                ", " + TABLE_ROW_ADDRESS +"," +
+                TABLE_ROW_DESC+","+TABLE_ROW_PHONE+
+                " from " +
+                TABLE_N_AND_A + " WHERE " +
+                TABLE_ROW_NAME + " = '" + name + "';";
+        Log.i("searchName() = ", query);
+        Cursor c = db.rawQuery(query, null);
+        return c;
+    }
+
+    // This class is created when our DataManager is initialized
+    private class CustomSQLiteOpenHelper extends SQLiteOpenHelper {
+        public CustomSQLiteOpenHelper(Context context) {
+            super(context, DbClass.DB_NAME, null, DB_VERSION);
+        }
+
+        // This method only runs the first time the database is created
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            // Create a table for photos and all their details
+            String newTableQueryString = "create table "
+                    + DbClass.TABLE_N_AND_A + " ("
+                    + DbClass.TABLE_ROW_ID + " integer primary key autoincrement not null,"
+                    + DbClass.TABLE_ROW_NAME
+                    + " text not null,"
+                    + DbClass.TABLE_ROW_ADDRESS
+                    + " text not null,"
+                    +DbClass.TABLE_ROW_DESC +"text,"
+                    +DbClass.TABLE_ROW_PHONE +"text);";
+            db.execSQL(newTableQueryString);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
     }
 }
-
--- app/src/main/java/com/yash/fooddonationapplication/reciepientSetup.java
+++ app/src/main/java/com/yash/fooddonationapplication/reciepientSetup.java
@@ -3,8 +3,11 @@ package com.yash.fooddonationapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +23,7 @@ public class reciepientSetup extends AppCompatActivity {
     private EditText orgPhone;
     private EditText orgDesc;
     private Button addOrgButton;
+    DbClass mydb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,17 +40,24 @@ public class reciepientSetup extends AppCompatActivity {
                 orgAddress = findViewById(R.id.orgSetAddress);
                 orgPhone = findViewById(R.id.orgSetupPhone);
                 orgDesc = findViewById(R.id.orgSetupDesc);
-                /*String orgNameText= orgName.getText().toString();
-                String orgAddressText = orgAddress.getText().toString();
-                String orgDescText = orgDesc.getText().toString();
-                String orgPhoneText = orgPhone.getText().toString();
-*/
-                SQLiteDatabase myDatabase = openOrCreateDatabase("OrgTable", MODE_PRIVATE, null);
-                DbClass mydb = null;
+
+                mydb = new DbClass(getApplicationContext());
+
+              /*  SQLiteDatabase myDatabase = openOrCreateDatabase("OrgTable", MODE_PRIVATE, null);
                 myDatabase.execSQL("CREATE TABLE IF NOT EXISTS orgs(name VARCHAR, address VARCHAR, description VARCHAR,phone INT(10))");
-                mydb.insertContact(orgName.getText().toString(), orgAddress.getText().toString(),orgPhone.getText().toString(), orgDesc.getText().toString());
+                mydb.insertContact(orgName.getText().toString(), orgAddress.getText().toString(),orgPhone.getText().toString(), orgDesc.getText().toString());*/
+                mydb.insert(orgName.getText().toString(),orgAddress.getText().toString(),orgDesc.getText().toString(),orgPhone.getText().toString());
+                showData(mydb.selectAll());
                 Toast.makeText(getApplicationContext(), "Data Saved", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(reciepientSetup.this, ReciepientList.class);
+                startActivity(intent);
             }
         });
         }
+    public void showData(Cursor c){
+        while (c.moveToNext()){
+            Log.i(c.getString(1), c.getString(2));
+        }
     }
+    }
+
