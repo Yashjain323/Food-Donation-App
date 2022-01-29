f686e6048e20ede4c493bdb817472bfa6e7df076
Tikam <tikamsingh1901900@gmail.com>
2022-01-29 17:16:36 +0530

    food_details_donor_Post

--- app/src/main/java/com/yash/fooddonationapplication/DonorFoodDetailsPost.java
+++ app/src/main/java/com/yash/fooddonationapplication/DonorFoodDetailsPost.java
@@ -3,8 +3,14 @@ package com.yash.fooddonationapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class DonorFoodDetailsPost extends AppCompatActivity {
+    EditText Quantity=findViewById(R.id.DonorFoodDetail_Quantity);
+    EditText Parishability=findViewById(R.id.DonorFoodDetail_Parishability);
+    EditText NutritionValue=findViewById(R.id.DonorFoodDetail_NutritionValue);
+    EditText PersonsFed=findViewById(R.id.DonorFoodDetail_PersonsFed);
+    EditText DonorFoodDetail=findViewById(R.id.DonorFoodDetail_MultiLine);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
